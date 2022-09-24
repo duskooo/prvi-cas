7d0b1196a3254f89d714c4d02819a32f288f9d29
duskooo <114296115+duskooo@users.noreply.github.com>
2022-09-24 15:30:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var buttonElement = document.getElementById('dugme')
+buttonElement.addEventListener('click',function() {
+    alert('Operi kosu')
+
+
+    for(i = 0;i<3;i++){
+        alert('broj' +i)
+    }
+});
